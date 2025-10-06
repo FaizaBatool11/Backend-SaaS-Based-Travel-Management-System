c7238f742e53ae7068438171343b9a942f9136a9
FaizaBatool11 <faizabatool28890@gmail.com>
2025-10-06 04:56:59 -0700

    update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,37 @@
 
-import dotenv from "dotenv"
+// import dotenv from "dotenv"
+// dotenv.config();
+// const config = {
+//   development: {
+//     username: process.env.DB_USER || "root",
+//     password: process.env.DB_PASSWORD || "faiza123",
+//     database: process.env.DB_NAME || "travel",
+//     host: process.env.DB_HOST || "127.0.0.1",
+//     port: process.env.DB_PORT || 3306,
+//     dialect: "mysql",
+//   },
+//   test: {
+//     username: process.env.DB_USER || "root",
+//     password: process.env.DB_PASSWORD || "faiza123",
+//     database: process.env.DB_NAME || "travel",
+//     host: process.env.DB_HOST || "127.0.0.1",
+//     port: process.env.DB_PORT || 3306,
+//     dialect: "mysql",
+//   },
+//   production: {
+//     username: process.env.DB_USER || "root",
+//     password: process.env.DB_PASSWORD || "faiza123",
+//     database: process.env.DB_NAME || "travel",
+//     host: process.env.DB_HOST || "127.0.0.1",
+//     port: process.env.DB_PORT || 3306,
+//     dialect: "mysql",
+//   },
+// };
+
+// export default config;
+import dotenv from "dotenv";
 dotenv.config();
+
 const config = {
   development: {
     username: process.env.DB_USER || "root",
@@ -19,12 +50,17 @@ const config = {
     dialect: "mysql",
   },
   production: {
-    username: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "faiza123",
-    database: process.env.DB_NAME || "travel",
-    host: process.env.DB_HOST || "127.0.0.1",
-    port: process.env.DB_PORT || 3306,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+      },
+    },
   },
 };
 
